84e40be975cde843f317a74183e2e160c2aa2d9f
neha <nbahuguna884@gmail.com>
2024-02-14 13:15:19 +0530

    add vnet

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "rg" {
+  count    = var.create_resource_group ? 1 : 0
+  name     = var.resource_group_name
+  location = var.location
+  tags     = merge({ "Name" = format("%s", var.resource_group_name) }, var.tags, )
+}
+
+#-------------------------------------
+# VNET Creation - Default is "true"
+#-------------------------------------
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnetwork_name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = var.vnet_address_space
+  dns_servers         = var.dns_servers
+  tags                = merge({ "Name" = format("%s", var.vnetwork_name) }, var.tags, )
+
+  dynamic "ddos_protection_plan" {
+    for_each = local.if_ddos_enabled
+
+    content {
+      id     = azurerm_network_ddos_protection_plan.ddos[0].id
+      enable = true
+    }
+  }
+}
